2b1d60644d92e37ae91a9431e7ed973a27b38bf8
riensWinoto <rienslw@outlook.com>
2019-05-08 15:02:34 +0700

    Split into multi file and code adjustment

--- PytomationMail.py
+++ PytomationMail.py
@@ -1,69 +1,26 @@
-import smtplib
-import sys
-import time
-from datetime import datetime
-
-# variable
-myName = "your name"
-myEmail = "your@email.com"
-myPass = "y0urP4s5w0rd"
-myEmailSMTP = "smtp.yourEmailProvider.com" #for gmail: smtp.gmail.com  for outlook: smtp-mail.outlook.com
-mySMTPPort = 587
-
-receiverNames = ["receiver name"]
-receiverEmails = ["receiver@email.com"]
-emailSubject = "This is Python Automation First Try"
-emailBody = """
-Hello there,
-This is me, Riens Winoto just try the research for automation \
-using Python.
-Python do the magic well
-
-Regards,
-Riens Winoto
-"""
-
-# function
-def inital_setup():
-
-    type(broadCaster)
-    broadCaster.ehlo()
-    broadCaster.starttls()
-    try:
-        broadCaster.login(myEmail, myPass)
-    except IOError as err:
-        print(str(err))
-        time.sleep(1.0)
-        sys.exit()
-
-
-def get_date_time():
-    date_and_time = datetime.now()
-    str_date_time = date_and_time.strftime('%b %-d,%Y, %-I:%M%p')
-    return str_date_time
-
+import pytomationvar as pymvar
+import pytomationfunc as pymfunc
 
 if __name__ == "__main__":
-    broadCaster = smtplib.SMTP(myEmailSMTP, mySMTPPort)
-    inital_setup()
-
+    broadCaster = pymfunc.initial_setup()
     count = 0
-    while count < len(receiverNames):
-        while count < len(receiverEmails):
+    while count < len(pymvar.receiverNames):
+        while count < len(pymvar.receiverEmails):
 
-            fromSender = "from:" + " " + myName + " " + "<" + myEmail + ">"
-            toReceiver = "to:" + " " + receiverNames[count] + " " + "<" + receiverEmails[count] + ">"
-            emailMessage = "subject:" + " " + emailSubject + "\n" + emailBody
+            fromSender = pymfunc.get_sender(pymvar.myName, pymvar.myEmail)
+            toReceiver = pymfunc.get_receiver(pymvar.receiverNames[count], pymvar.receiverEmails[count])
+            emailMessage = pymfunc.get_email_message(pymvar.emailSubject, pymvar.emailBody)
             messenger = fromSender + "\n" + toReceiver + "\n" + emailMessage
 
-            broadCaster.sendmail(myEmail, receiverEmails[count], messenger)
-            sendDateTime = get_date_time()
+            broadCaster.sendmail(pymvar.myEmail, pymvar.receiverEmails[count], messenger)
+            sendDateTime = pymfunc.get_date_time()
 
-            print("e-mail sent successfully to {} at {} \n".format(receiverNames[count], sendDateTime))
-            count += 1
-            if count >= len(receiverNames) or count >= len(receiverEmails):
+            print("e-mail sent successfully to {} at {} \n".format(pymvar.receiverNames[count], sendDateTime))
+
+            if count >= len(pymvar.receiverNames) or count >= len(pymvar.receiverEmails):
                 break
             else:
+                count += 1
                 continue
         continue
     broadCaster.quit()
